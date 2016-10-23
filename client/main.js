8f34aaabed0016591fe2451bad216ef2f21a762b
PierreGallet <pierre.gallet@hotmail.fr>
2016-10-23 14:13:11 +0200

    adding login

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
+import { ReactiveVar } from 'meteor/reactive-var'
+import '../imports/startup/accounts-config.js'
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
